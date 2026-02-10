69233c4ec37cade5b027f24971dc3753f98f6c84
Vyshak V <vyshakvijay@gmail.com>
2026-02-10 16:42:14 +0530

    Enemy Player Collision Fix

---
+++ Assets/Scripts/Items/ItemManagerRefactored.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManagerRefactored : MonoBehaviour
+{
+    public void AddItem(ItemSO itemData)
+    {
+
+    }
+}
