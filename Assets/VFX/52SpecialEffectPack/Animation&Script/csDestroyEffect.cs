e4c97a4ff78fbb979532581e564f627ded8102d2
Vyshak V <vyshakvijay@gmail.com>
2026-03-11 01:40:45 +0530

    Merged Branches and Game Fixes

--- Assets/VFX/52SpecialEffectPack/Animation&Script/csDestroyEffect.cs
+++ Assets/VFX/52SpecialEffectPack/Animation&Script/csDestroyEffect.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class csDestroyEffect : MonoBehaviour {
+namespace Test
+{
+	public class csDestroyEffect : MonoBehaviour
+	{
 
-	void Update ()
-    {
-	    if(Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.C))
-        {
-            Destroy(gameObject);
-        }
+		void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.C))
+			{
+				Destroy(gameObject);
+			}
+		}
 	}
+
 }
