9798d1db5343e01c4c949f197cee3fccde29d029
Vyshak V <vyshakvijay@gmail.com>
2026-08-15 05:17:27 +0530

    Fixed Solar System and Acid Rain Weapon Functionality

--- Assets/_Code/Weapon/WeaponSystems/SolarSystem.cs
+++ Assets/_Code/Weapon/WeaponSystems/SolarSystem.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SolarSystem : MonoBehaviour
+public class SolarSystem : WeaponBase
 {
     [Header("Solar System Settings")]
     public List<GameObject> planetPrefabs; // Assign different planet prefabs in Inspector
@@ -12,11 +12,15 @@ public class SolarSystem : MonoBehaviour
     public float maxOrbitSpeed = 100f;     // Speed of first planet
     public float speedSubtractor = 10f;    // How much slower each next planet gets
 
+    public int damageIncWithLvl = 5;
+
     private Transform player;
-    private readonly List<PlanetData> planets = new List<PlanetData>();
+    public List<PlanetData> planets = new List<PlanetData>();
+
+    public List<GameObject> activePlanets = new List<GameObject>();
 
     [System.Serializable]
-    private class PlanetData
+    public class PlanetData
     {
         public GameObject planet;
         public float angle;
@@ -24,7 +28,7 @@ public class SolarSystem : MonoBehaviour
         public float radius;
     }
 
-    void Start()
+    protected override void Start()
     {
         player = transform.parent; // Weapon is attached to Player
 
@@ -54,20 +58,141 @@ public class SolarSystem : MonoBehaviour
                 speed = calculatedSpeed,
                 radius = radius
             });
+
+            if(level >= i + 1)
+                activePlanets.Add(planet);
+            else
+                planet.SetActive(false);
         }
     }
 
-    void Update()
+    public override void SpawnWeapon(Transform parent)
     {
-        if (player == null) return;
+        base.SpawnWeapon(parent);
+    }
 
-        foreach (var data in planets)
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        AOESize = statManager.GetStat(EStatType.AOESize).currentValue;
+        cooldown = statManager.GetStat(EStatType.AttackCooldown).currentValue;
+        duration = statManager.GetStat(EStatType.ActiveDuration).currentValue;
+        projectileSpeed = statManager.GetStat(EStatType.ProjectileSpeed).currentValue;
+        projectileCount = statManager.GetStat(EStatType.ProjectileCount).currentValue;
+    }
+
+    public override void UpdateWeapon()
+    {
+        base.UpdateWeapon();
+        UpdatePlanetRotation();
+    }
+
+    public override void ToggleGFXVisibility(bool b)
+    {
+        foreach(GameObject planet in activePlanets)
         {
-            data.angle += data.speed * Time.deltaTime;
-            if (data.angle > 360f) data.angle -= 360f;
+            planet.SetActive(b);
+        }
+    }
 
-            Vector3 offset = Quaternion.Euler(0, 0, data.angle) * Vector3.right * data.radius;
-            data.planet.transform.position = player.position + offset;
+    protected override void UpdateStatsHandled()
+    {
+        AOESize = statManager.GetStat(EStatType.AOESize).currentValue;
+        cooldown = statManager.GetStat(EStatType.AttackCooldown).currentValue;
+        duration = statManager.GetStat(EStatType.ActiveDuration).currentValue;
+        projectileSpeed = statManager.GetStat(EStatType.ProjectileSpeed).currentValue;
+        projectileCount = statManager.GetStat(EStatType.ProjectileCount).currentValue;
+    }
+
+    public override void UpdateWeaponDamage()
+    {
+        base.UpdateWeaponDamage();
+    }
+
+    public override void LevelUpWeapon()
+    {
+        base.LevelUpWeapon();
+
+        UpgradeSolarSystem();
+    }
+
+    private void UpdatePlanetRotation()
+    {
+        if (player == null || inCooldown) return;
+
+        for (int i = 0; i < activePlanets.Count; i++)
+        {
+            planets[i].angle += planets[i].speed * Time.deltaTime;
+            if (planets[i].angle > 360f) planets[i].angle -= 360f;
+
+            Vector3 offset = Quaternion.Euler(0, 0, planets[i].angle) * Vector3.right * planets[i].radius;
+            planets[i].planet.transform.position = player.position + offset;
+        }
+    }
+
+    private void UpgradeSolarSystem()
+    {
+        switch (level)
+        {
+            case 1:
+                break;
+            case 2:
+                statManager.ModifyStatValue(EStatType.ProjectileCount, 1);
+                statManager.ModifyStatValue(EStatType.AOESize, 1);
+                break;
+            case 3:
+                statManager.ModifyStatValue(EStatType.ProjectileCount, 1);
+                statManager.ModifyStatValue(EStatType.AOESize, 1);
+                break;
+            case 4:
+                statManager.ModifyStatValue(EStatType.ProjectileCount, 1);
+                statManager.ModifyStatValue(EStatType.AOESize, 1);
+                statManager.ModifyStatValue(EStatType.ActiveDuration, 2.5f);
+                break;
+            case 5:
+                statManager.ModifyStatValue(EStatType.ProjectileCount, 1);
+                statManager.ModifyStatValue(EStatType.AOESize, 1);
+                break;
+            case 6:
+                statManager.ModifyStatValue(EStatType.ProjectileCount, 1);
+                statManager.ModifyStatValue(EStatType.AOESize, 1);
+                break;
+            case 7:
+                statManager.ModifyStatValue(EStatType.ProjectileCount, 1);
+                statManager.ModifyStatValue(EStatType.AOESize, 1);
+                break;
+            case 8:
+                statManager.ModifyStatValue(EStatType.ProjectileCount, 1);
+                statManager.ModifyStatValue(EStatType.AOESize, 1);
+                statManager.ModifyStatValue(EStatType.ActiveDuration, 2.5f);
+                break;
+            case 9:
+                statManager.ModifyStatValue(EStatType.ProjectileCount, 1);
+                statManager.ModifyStatValue(EStatType.AOESize, 1);
+                break;
+            default:
+                Debug.Log($"Max Level Reached for {weaponData.weaponName}");
+                break;
+        }
+
+        for (int i = 0; i < planets.Count; i++)
+        {
+            if (level >= i + 1)
+            {
+                if (!activePlanets.Contains(planets[i].planet))
+                    activePlanets.Add(planets[i].planet);
+            }
         }
     }
 }
